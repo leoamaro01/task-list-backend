b272499c52f9eb0ff5609f2343831d20380ef4df
PixelDG <leoamaro2001@gmail.com>
2025-02-26 05:21:30 -0500

    feat: using PORT env var for port assignment
    8080 is used as a default

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { bootstrap } from "./app.js";
 
 try {
-  const { url } = await bootstrap();
+  const { url } = await bootstrap(+(process.env.PORT || 8080));
   console.log(`Server started at ${url}`);
 } catch (e) {
   console.error(e);
